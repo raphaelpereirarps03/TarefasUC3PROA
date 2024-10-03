40b34f554fa1985decd5c216265c8dbca394d238
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-02 23:14:42 -0300

    Adicionando Projeto no Banco de Dados e Mensagens de Erro e Sucesso

--- React - Estudo/Costs/costs/src/components/Pages/NewProject.js
+++ React - Estudo/Costs/costs/src/components/Pages/NewProject.js
@@ -1,11 +1,48 @@
 import styles from './NewProject.module.css';
+
+
+// Não se usa mais o useHistory, agora é o useNavigate
+import {useNavigate } from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm';
+
 function NewProject () {
+    const navigate = useNavigate()
+
+    function createPost(project) {
+        
+        // initialize cost and service
+
+        project.cost = 0;
+        project.services=[]
+
+
+        fetch ("http://localhost:5000/projects", {
+            method:"Post",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp=>resp.json()))
+        .then((data)=>(
+            console.log(data)
+            //redirect
+            
+            (navigate('/projects', { state: { message: 'Projeto criado com sucesso' } })
+        )
+
+        ))
+        .catch(err=>console.log(err))
+
+    }
+
+
     return (
         <div className={styles.newproject_container}>
             <h1> Criar projeto </h1>
             <p> Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 }
--- React - Estudo/Costs/costs/src/components/project/ProjectForm.js
+++ React - Estudo/Costs/costs/src/components/project/ProjectForm.js
@@ -7,10 +7,11 @@ import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
 
     const [categories, setCategories] = useState([]);
-
+    const [project, setProject] = useState(projectData || {})
+ 
     useEffect(() => {
         /* 
             Fazemos uma requisição à API usando fetch para a URL"https://localhost:5000/categories". 
@@ -30,24 +31,50 @@ function ProjectForm({ btnText }) {
     }, [])
 
 
+    const submit = (e) => {
+        e.preventDefault();
+        //console.log(project);
+        handleSubmit(project);
+    }
+
+    function handleChange(e) {
+        setProject({...project, [e.target.name]: e.target.value})
+
+    }
+
+    function handleCategory(e) {
+        setProject({...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text
+        }})
+
+    }
+
+
     return (
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
             <Input
                 type="text"
                 text="Nome do projeto"
                 name="name"
                 placeholder="Insira o nome do projeto"
+                handleOnChange={handleChange}
+                value={project.name ? project.name : ''}
             />
             <Input
                 type="number"
                 text="Orçamento do projeto"
                 name="budge"
                 placeholder="Insira o orçamento do projeto"
+                handleOnChange={handleChange}
+                value={project.budge ? project.budge : ''}
             />
             <Select
                 name="category_id"
                 text="Selecione uma categoria"
                 options={categories}
+                handleOnChange={handleCategory}
+                value={project.category ? project.category.id : ''}
             />
             <SubmitButton text={btnText} />
 
